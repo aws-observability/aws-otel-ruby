ae893f8a279d033dc641f03bbac336f5b1de7725
github-action-benchmark <github@users.noreply.github.com>
2022-02-10 20:11:49 +0000

    add Soak Test Results - sample-app-ruby-on-rails-manual (customSmallerIsBetter) benchmark result for 285380ae53a51ed620820ad4bbc6be864d7127a6

---
+++ soak-tests/per-commit-overall-results/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1644523909314,
+  "repoUrl": "https://github.com/aws-observability/aws-otel-ruby",
+  "entries": {
+    "Soak Test Results - sample-app-ruby-on-rails-manual": [
+      {
+        "commit": {
+          "author": {
+            "name": "Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "nathanielruiz98@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "285380ae53a51ed620820ad4bbc6be864d7127a6",
+          "message": "Fix Soak Test scripts paths (#13)",
+          "timestamp": "2022-02-09T23:58:28Z",
+          "url": "https://github.com/aws-observability/aws-otel-ruby/commit/285380ae53a51ed620820ad4bbc6be864d7127a6"
+        },
+        "date": 1644523908953,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 10.512470238095238,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 865.5814924568965,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ]
+  }
+}
